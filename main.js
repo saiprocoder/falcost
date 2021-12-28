b9c5935b8070f572791f3300cadf3b8948323d70
saiprocoder <95332916+saiprocoder@users.noreply.github.com>
2021-12-28 10:36:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var a=prompt("Welcome To Falcon! If you want to see our site of Falcon, Type Yes. If No, type No.");
+  	if(a== "Yes")
+  	{
+  	    window.location.href="https://bit.ly/falcost";
+  	}
+    if(a== "No")
+  	{
+  	    window.location.href="index.html";
+  	}
